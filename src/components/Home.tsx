e7d39b977e1e29557a09e065ec65203a5d044e2c
Mary Boman <mboman28@gmail.com>
2024-08-05 20:11:06 -0400

    CN buttons and about/help links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import { Button, styled } from "@mui/material";
 import { useNavigate } from "react-router";
 
 import { Container } from "./shared"
+import { Link } from "react-router-dom";
 
 type HomeProps = {
 }
@@ -15,18 +16,46 @@ const Home: React.FC<HomeProps> = () => {
             <p>To get started, choose a proposition from the menu on the left.</p>
             <p>Or, start with one of the axioms:</p>
             <ButtonContainer>
-                <Button onClick={() => {navigate('/?p=A0.1')}}>Axiom 1</Button>
-                <Button onClick={() => {navigate('/?p=A0.2')}}>Axiom 2</Button>
-                <Button onClick={() => {navigate('/?p=A0.3')}}>Axiom 3</Button>
-                <Button onClick={() => {navigate('/?p=A0.4')}}>Axiom 4</Button>
-                <Button onClick={() => {navigate('/?p=A0.5')}}>Axiom 5</Button>
+                <AxiomButton onClick={() => {navigate('/?p=A0.1')}}>Axiom 1</AxiomButton>
+                <AxiomButton onClick={() => {navigate('/?p=A0.2')}}>Axiom 2</AxiomButton>
+                <AxiomButton onClick={() => {navigate('/?p=A0.3')}}>Axiom 3</AxiomButton>
+                <AxiomButton onClick={() => {navigate('/?p=A0.4')}}>Axiom 4</AxiomButton>
+                <AxiomButton onClick={() => {navigate('/?p=A0.5')}}>Axiom 5</AxiomButton>
             </ButtonContainer>
+            <p>Or Common Notions:</p>
+            <ButtonContainer>
+                <CNButton onClick={() => {navigate('/?p=N0.1')}}>Common Notion 1</CNButton>
+                <CNButton onClick={() => {navigate('/?p=N0.2')}}>Common Notion 2</CNButton>
+                <CNButton onClick={() => {navigate('/?p=N0.3')}}>Common Notion 3</CNButton>
+                <CNButton onClick={() => {navigate('/?p=N0.4')}}>Common Notion 4</CNButton>
+                <CNButton onClick={() => {navigate('/?p=N0.5')}}>Common Notion 5</CNButton>
+            </ButtonContainer>
+            <p>To learn how to use this app, visit the <Link to='/help'>help page</Link>.</p>
+            <p>For more information on this project, visit the <Link to='/about'>about page</Link>.</p>
         </HomeContainer>
     );
 }
 
+const NodeButton = styled(Button)({
+    color: 'black',
+    textTransform: 'none',
+
+    margin: '0 10px',
+    padding: '5px 10px',
+});
+
+const AxiomButton = styled(NodeButton)({
+    backgroundColor: 'lightblue',
+});
+
+const CNButton = styled(NodeButton)({
+    backgroundColor: 'pink',
+});
+
 const ButtonContainer = styled('div')({
+    backgroundColor: 'white',
     display: 'flex',
+    width: 'fit-content',
 })
 
 const HomeContainer = styled(Container)({
