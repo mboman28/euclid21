5b012481787b47eb8d7da4f102b3747fe354c0a5
Mary Boman <mboman28@gmail.com>
2024-08-05 21:14:56 -0400

    clean up unused items

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import { pdfjs } from 'react-pdf';
 import {
   HashRouter,
@@ -35,8 +34,3 @@ root.render(
     </DataContext.Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
